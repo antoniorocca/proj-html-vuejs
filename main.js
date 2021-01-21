e0be82f6841279fc91947f35acc0a30d0581082f
antonio <antoniorocca46@gmail.com>
2021-01-21 15:25:43 +0100

    Third Commit

---
+++ main.js
@@ -0,0 +1,74 @@
+let app = new Vue({
+  el: "#app2",
+  data: {
+    col1: {
+      logo: "./imgs/logo-footer.png",
+      contacts: [
+        "12345 North Main Street,",
+        "New York, NY 555555",
+        "Phone: 1.800.555.6789",
+        "Email: info@company.com",
+        "Web: Theme-Fusion.com",
+      ],
+      socials: [
+        { social: "fab fa-facebook-f", 
+          link: "https://www.facebook.com", 
+        },
+        { social: "fab fa-twitter", 
+          link: "https://www.twitter.com", 
+        },
+        { social: "fab fa-instagram", 
+          link: "https://www.instagram.com", 
+        },
+        {
+          social: "fab fa-youtube", 
+          link: "https://www.youtube.com",
+        },
+      ],
+    },
+    col2: {
+      title: "TOP RATED PRODUCTS",
+      info: [
+        {
+          description: "Leather Gloves",
+          price: "$45", 
+          picture: "./imgs/leather_gloves.jpg", 
+        },
+        {
+          description: "Black Leather Jacket",
+          discount: "$235",
+          price: "$200",
+          picture: "./imgs/black-elegant-jacket.jpg",
+        },
+        {
+          description: "Spring Printed Dress",
+          price: "$47",
+          picture: "./imgs/spring-dress.jpg",
+        },
+      ],
+    },
+    col3: {
+      title: "RECENT POSTS",
+      text: [
+        "Aenean lobortis sapien enim viverra",
+        "Duis ac massa semper maximus",
+        "Nunc fermint nulla eu justo sem id",
+        "Aliquam posuere magna eget nibh",
+        "Cras ac nulla ac consecte rutrum",
+        "Fusce mattis nunc ut aliquam",
+      ],
+    },
+    col4: {
+      title: "TAGS",
+      tags:[
+        ["Black", "boots", "Brown", "Casual"],
+        ["D&G", "Fabric", "flowers", "Grey"],
+        ["hat", "Hipster", "lines"],
+        ["multi-purpose", "New York"],
+        ["Outdoors", "red", "responsive"],
+        ["summer", "sweater", "Travel"],
+        ["Warm", "White", "winter"],
+      ]
+    },
+  },
+});
